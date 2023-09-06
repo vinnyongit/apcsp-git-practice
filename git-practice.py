8532ae7f65e9ec78381ac2722a0c5a03499f8e9d
vinnyongit <144164943+vinnyongit@users.noreply.github.com>
2023-09-06 11:21:21 -0700

    Update git-practice.py

--- git-practice.py
+++ git-practice.py
@@ -1,1 +1,12 @@
+toDoList = ["Math Homework", "Cook Dinner", "Fold Laundry"]
+
+def addItem(item):
+   # Finish this code
+   return toDoList
+
+userAns = input("Do you want to add to your list or quit? A/Q")
+while userAns == "A":
+   item = input("What item do you want to add?")
+   addItem(item)
+   userAns = input("Do you want to add to your list or quit? A/Q")
 
